cfa1a542bc3811ef6e36e933cf7fe6b77109498c
diogoX451 <diogo.g07@icloud.com>
2023-10-10 07:41:50 -0300

    ordenação

---
+++ Ordenation/quicksort.go
@@ -0,0 +1,31 @@
+package ordenation
+
+func QuickSort(array []int, left int, right int) {
+	if left < right {
+		pivot := partition(array, left, right)
+		QuickSort(array, left, pivot-1)
+		QuickSort(array, pivot+1, right)
+	}
+}
+
+func partition(array []int, left int, rigth int) int {
+	pivot := array[left]
+	pointer := left
+
+	for i := left + 1; i <= rigth; i++ {
+		if array[pointer] <= pivot {
+			pointer++
+			replacements(array, i, pointer)
+		}
+	}
+	
+	replacements(array, left, pointer)
+
+	return pointer
+}
+
+func replacements(array []int, i int, j int) {
+	aux := array[i]
+	array[i] = array[j]
+	array[j] = aux
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diogoX451
+
+go 1.21.0
